b87eaa0473dacf3f67160557cf352dd888862235
imsoo <smkongdo@gmail.com>
2019-11-06 00:10:29 +0900

    클라이언트와 서버간 [SEQ | FRAME] 형태로 통신하도록 수정
    순서번호를 포함하여 프레임을 주고 받도록 수정하여 분산처리되어
    도착한 프레임들을 순서에 맞게 유지할수 있도록 구현 함.
    클라이언트 측에서는 우선순위 큐를 사용하여 프레임을 순서에맞게 유지 함.
    
    향후 서버와 다크넷 간 공유메모리를 이용한 통신 구조를 ZMQ IPC로 변경해보고
    성능 향상 여부를 확인할 계획임.

--- client/zmq_client/main.cpp
+++ client/zmq_client/main.cpp
@@ -4,184 +4,158 @@
 #include <opencv2/opencv.hpp>
 #include <thread>
 #include <queue>
-#include <mutex>
-#include <condition_variable>
 #include <chrono>
 #include <thread>
+#include <concurrent_priority_queue.h>
+#include "share_queue.h"
+
 
 using namespace cv;
 using namespace std;
 
-template <typename T>
-class SharedQueue
-{
-public:
-	SharedQueue();
-	~SharedQueue();
 
-	T& front();
-	void pop_front();
-
-	void push_back(const T& item);
-	void push_back(T&& item);
-
-	int size();
-	bool empty();
-
-private:
-	std::deque<T> queue_;
-	std::mutex mutex_;
-	std::condition_variable cond_;
-};
-
-template <typename T>
-SharedQueue<T>::SharedQueue() {}
-
-template <typename T>
-SharedQueue<T>::~SharedQueue() {}
-
-template <typename T>
-T& SharedQueue<T>::front()
-{
-	std::unique_lock<std::mutex> mlock(mutex_);
-	while (queue_.empty())
-	{
-		cond_.wait(mlock);
-	}
-	return queue_.front();
-}
-
-template <typename T>
-void SharedQueue<T>::pop_front()
-{
-	std::unique_lock<std::mutex> mlock(mutex_);
-	while (queue_.empty())
-	{
-		cond_.wait(mlock);
-	}
-	queue_.pop_front();
-}
-
-template <typename T>
-void SharedQueue<T>::push_back(const T& item)
-{
-	std::unique_lock<std::mutex> mlock(mutex_);
-	queue_.push_back(item);
-	mlock.unlock();     // unlock before notificiation to minimize mutex con
-	cond_.notify_one(); // notify one waiting thread
-
-}
-
-template <typename T>
-void SharedQueue<T>::push_back(T&& item)
-{
-	std::unique_lock<std::mutex> mlock(mutex_);
-	queue_.push_back(std::move(item));
-	mlock.unlock();     // unlock before notificiation to minimize mutex con
-	cond_.notify_one(); // notify one waiting thread
-
-}
-
-template <typename T>
-int SharedQueue<T>::size()
-{
-	std::unique_lock<std::mutex> mlock(mutex_);
-	int size = queue_.size();
-	mlock.unlock();
-	return size;
-}
-
+// thread
+void fetch_thread(void);
 void capture_thread(void);
 void recv_thread(void);
 void output_show_thread(void);
 void input_show_thread(void);
-static void onMouse(int event, int x, int y, int, void*);
-
-
-void *context;
-void *requester;
-volatile int requester_idx = 0;
-zmq_pollitem_t items[1];
-const int REQUESTER_NUM = 1;
-SharedQueue<Mat> recv_queue;
-SharedQueue<Mat> cap_queue;
-VideoCapture cap;
 volatile bool exit_flag = false;
+volatile bool fetch_flag = false;
 
+// ZMQ
+void *context;
+void *sock_push_server;
+void *sock_pull_server;
+
+// pair
+class ComparePair
+{
+public:
+	bool operator()(pair<long, void *> n1, pair<long, void *> n2) {
+		return n1.first > n2.first;
+	}
+};
+
+// Queue
+//SharedQueue<Mat> recv_queue;
+SharedQueue<Mat> cap_queue;
+SharedQueue<Mat> fetch_queue;
+concurrency::concurrent_priority_queue<pair<long, void*>, ComparePair> recv_queue;
+
+
+// opencv
+static void onMouse(int event, int x, int y, int, void*);
+VideoCapture cap;
 static Mat mat_show_output;
 static Mat mat_show_input;
 static Mat mat_recv;
 static Mat mat_cap;
+static Mat mat_fetch;
 
 const int cap_width = 640;
 const int cap_height = 480;
+double delay;
 
 int main()
 {
-
-	//  Socket to talk to clients
+	// ZMQ
 	context = zmq_ctx_new();
-	requester = zmq_socket(context, ZMQ_DEALER);
-	zmq_setsockopt(requester, ZMQ_IDENTITY, "ims", 3);
-	zmq_connect(requester, "tcp://104.199.144.166:5570");
 
-	items[0] = { static_cast<void*>(requester), 0, ZMQ_POLLIN, 0 };
-	long long count = 0;
+	sock_push_server = zmq_socket(context, ZMQ_PUSH);
+	zmq_connect(sock_push_server, "tcp://104.199.144.166:5575");
+
+	sock_pull_server = zmq_socket(context, ZMQ_PULL);
+	zmq_connect(sock_pull_server, "tcp://104.199.144.166:5570");
+
+
 	//비디오 캡쳐 초기화
-	cap = VideoCapture("C:\\Users\\COMSE\\source\\repos\\zmq_test\\x64\\Release\\r2.f135.mp4");
-	cap.set(CV_CAP_PROP_BUFFERSIZE, 3);
-	//cap = VideoCapture(0);
+	//cap = VideoCapture("C:\\Users\\COMSE\\source\\repos\\zmq_test\\x64\\Release\\road.mp4");
+
+	cap = VideoCapture(0);
 
 	if (!cap.isOpened()) {
 		cerr << "Erro VideoCapture.\n";
 		return -1;
 	}
 
-	// 동영상 프레임 읽어오기
-	cap.read(mat_cap);
+	double fps = cap.get(CAP_PROP_FPS);
+	delay = (1000.0 / fps) / 2.0;
 
-	if (mat_cap.empty()) {
+	
+	// 동영상 프레임 읽어오기
+	cap.read(mat_fetch);
+
+	if (mat_fetch.empty()) {
 		cerr << "빈 영상이 캡쳐되었습니다.\n";
 		return 0;
 	}
 
-	mat_show_output = mat_cap.clone();
-	mat_show_input = mat_cap.clone();
+	mat_show_output = mat_fetch.clone();
+	mat_show_input = mat_fetch.clone();
+	
+	// thread 설정
+	thread thread_fetch(fetch_thread);
+	thread_fetch.detach();
 
-	// 잠깐 대기
-	std::chrono::duration<int, std::milli> timespan(1000);
-	std::this_thread::sleep_for(timespan);
+	while (!fetch_flag);
 
-	thread thread_capture(capture_thread);
-	thread thread_recv(recv_thread);
 	thread thread_show_input(output_show_thread);
 	thread thread_show_output(input_show_thread);
+	thread thread_recv(recv_thread);
+	thread thread_capture(capture_thread);
 
-	thread_capture.detach();
-	thread_recv.detach();
 	thread_show_input.detach();
 	thread_show_output.detach();
+	thread_recv.detach();
+	thread_capture.detach();
 
 	while (!exit_flag)
 	{
-		cout << recv_queue.size() << endl;
+		cout << recv_queue.size() << " " << delay <<  endl;
 	}
 
-	zmq_close(requester);
+	cap.release();
+
+	zmq_close(sock_pull_server);
+	zmq_close(sock_push_server);
 	zmq_ctx_destroy(context);
 
 	return 0;
 }
 
 #define BUF_LEN 256000
+#define FETCH_THRESH 0
+void fetch_thread(void) {
+	while (!exit_flag) {
+		// 동영상 프레임 읽어오기
+		if (cap.grab()) {
+			cap.retrieve(mat_fetch);
+
+			// fetch 큐에 삽입
+			fetch_queue.push_back(mat_fetch.clone());
+
+			if (fetch_queue.size() > FETCH_THRESH)
+				fetch_flag = true;
+		}
+		// 
+		else {
+			fetch_flag = true;
+			return;
+		}
+	}
+}
 
 void capture_thread(void) {
-	static vector<int> param = { IMWRITE_JPEG_QUALITY, 30 };
+	static vector<int> param = { IMWRITE_JPEG_QUALITY, 75 };
 	static vector<uchar> encode_buf(BUF_LEN);
+	int frame_seq_num = 1;
+	string frame_seq;
 
 	while (!exit_flag) {
 		// 동영상 프레임 읽어오기
-		cap.read(mat_cap);
+		mat_cap = fetch_queue.front();
+		fetch_queue.pop_front();
 
 		if (mat_cap.empty()) {
 			cerr << "빈 영상이 캡쳐되었습니다.\n";
@@ -192,26 +166,34 @@ void capture_thread(void) {
 		resize(mat_cap, mat_cap, Size(cap_width, cap_height));
 
 		// 캡처 큐에 삽입
-		cap_queue.push_back(mat_cap);
+		cap_queue.push_back(mat_cap.clone());
 
 		// jpg 인코딩
 		imencode(".jpg", mat_cap, encode_buf, param);
 
 		// 서버로 전송
-		//zmq_send(requester, "ims", 3, ZMQ_SNDMORE);
-		zmq_send(requester, "", 0, ZMQ_SNDMORE);
-		zmq_send(requester, &encode_buf[0], encode_buf.size(), ZMQ_NOBLOCK);
+		frame_seq = to_string(frame_seq_num);
+		zmq_send(sock_push_server, frame_seq.c_str(), frame_seq.length(), ZMQ_SNDMORE);
+		zmq_send(sock_push_server, &encode_buf[0], encode_buf.size(), 0);
+		frame_seq_num++;
 	}
 }
 
+#define MAX_SEQ_NUM 20
 void recv_thread(void) {
 	static vector<uchar> decode_buf(BUF_LEN);
-	while (!exit_flag) {
-		zmq_poll(&items[0], 1, -1);
-		// 데이터 수신
-		if (items[0].revents & ZMQ_POLLIN) {
-			zmq_recv(requester, &decode_buf[0], BUF_LEN, ZMQ_NOBLOCK);
+	unsigned char seq_buf[MAX_SEQ_NUM] = { 0 };
+	int recv_seq_len;
+	int recv_msg_len;
+	int frame_seq_num = 1;
 
+	while (!exit_flag) {
+
+		recv_seq_len = zmq_recv(sock_pull_server, seq_buf, MAX_SEQ_NUM, ZMQ_NOBLOCK);
+
+		if (recv_seq_len > 0) {
+			frame_seq_num = atoi((const char *)seq_buf);
+			recv_msg_len = zmq_recv(sock_pull_server, &decode_buf[0], BUF_LEN, ZMQ_NOBLOCK);
 			// 디코딩
 			mat_recv = imdecode(decode_buf, IMREAD_COLOR);
 
@@ -221,8 +203,10 @@ void recv_thread(void) {
 				// 동영상 프레임 크기 조정
 				resize(mat_recv, mat_recv, Size(cap_width, cap_height));
 
-				// 수신 큐에 삽입
-				recv_queue.push_back(mat_recv);
+				// 수신 큐(우선순위 큐) 에 삽입
+				// Mat 동적 할당함.
+				pair<long, void *> p = make_pair(frame_seq_num, new Mat(mat_recv));
+				recv_queue.push(p);
 			}
 		}
 	}
@@ -231,8 +215,10 @@ void recv_thread(void) {
 #define DONT_SHOW 0
 #define DONT_SHOW_THRESH 1
 #define SHOW_START 1
-#define SHOW_START_THRESH 10
+#define SHOW_START_THRESH 2
+
 int volatile show_state = DONT_SHOW;
+int volatile show_frame = 1;
 void input_show_thread(void) {
 	cvNamedWindow("INPUT");
 	moveWindow("INPUT", 0, 0);
@@ -259,8 +245,12 @@ void input_show_thread(void) {
 		cv::imshow("INPUT", mat_show_input);
 
 		// ESC 키를 입력하면 루프가 종료됩니다. 
-		if (waitKey(10) >= 0)
+		if (waitKey(1) >= 0)
 			exit_flag = true;
+
+		// 잠깐 대기
+		std::chrono::duration<double, std::milli> timespan(delay);
+		std::this_thread::sleep_for(timespan);
 	}
 }
 
@@ -280,8 +270,22 @@ void output_show_thread(void) {
 			break;
 		case SHOW_START:
 			if (recv_queue.size() >= DONT_SHOW_THRESH) {
-				mat_show_output = recv_queue.front();
-				recv_queue.pop_front();
+
+				pair<long, void *> p;
+				// pop 성공
+				if (recv_queue.try_pop(p)) {
+					// 순서에 맞는 프레임인 경우 꺼내서 출력
+					if (p.first == show_frame) {
+						mat_show_output = ((Mat *)p.second)->clone();
+						delete p.second;
+						show_frame++;
+					}
+					// 아닌 경우 다시 삽입
+					else {
+						show_state = DONT_SHOW;
+						recv_queue.push(p);
+					}
+				}
 			}
 			else {
 				show_state = DONT_SHOW;
@@ -295,8 +299,13 @@ void output_show_thread(void) {
 		cv::imshow("OUTPUT", mat_show_output);
 
 		// ESC 키를 입력하면 루프가 종료됩니다. 
-		if (waitKey(10) >= 0)
+		if (waitKey(1) >= 0)
 			exit_flag = true;
+
+		// 잠깐 대기
+		std::chrono::duration<double, std::milli> timespan(delay);
+		std::this_thread::sleep_for(timespan);
+
 	}
 }
 
